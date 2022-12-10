1278ad37a7b751e911e609a388a620715212b7b3
Michael Hirschmugl <michael.hirschmugl@gmx.at>
2022-12-10 13:03:26 +0100

    Added a sys path in order to load modules correctly.

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ Complete project details: http://randomnerdtutorials.com
 import RPi.GPIO as GPIO
 from flask import Flask, render_template, request
 app = Flask(__name__)
+import sys
+sys.path.insert(0, './raspi_gpio_control')
 from raspi_gpio_control.read_state_log import raspi_gio_read_state_log
 from raspi_gpio_control.set_pin import raspi_gpio_control
 import time
